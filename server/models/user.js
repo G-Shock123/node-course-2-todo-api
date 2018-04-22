366063e390a1525a99ceb6aca77bdfcce2d31e03
Konrad Hylton <konradhylton@Konrads-MacBook-Pro.local>
2018-04-22 06:08:26 -0500

    ADD DELETE users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -56,6 +56,17 @@ UserSchema.methods.generateAuthToken = function () {
 
 };
 
+UserSchema.methods.removeToken = function(token){
+  var user =this;
+  return user.update({
+    $pull:{
+      tokens:{
+        token:token
+      }
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function(token)  {
   var User = this;
   var decoded;
@@ -75,6 +86,8 @@ UserSchema.statics.findByToken = function(token)  {
     'tokens.access': 'auth'
   });
 };
+
+
 
 UserSchema.statics.findByCredentials = function(email,password){
   var User = this;
